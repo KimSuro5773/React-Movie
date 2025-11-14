3470961a0c4a20b160e01982592fe083d0a9ce98
Kimsuro <ss01056489656@gmail.com>
2025-11-14 19:18:13 +0900

    feat: AppSlider 구현
    - 메뉴 버튼 클릭시 AppSlider가 나옴
    - overlay 추가로 AppSlider가 열림 상태일시 아무 곳이나 클릭해도 닫힘

--- src/components/common/AppHeader.tsx
+++ src/components/common/AppHeader.tsx
@@ -1,33 +1,47 @@
 import { Menu, Search } from "lucide-react";
 import { Button, Input } from "../ui";
+import AppSlider from "./AppSlider";
+import { useState } from "react";
 
 function AppHeader() {
+  const [isSliderOpen, setIsSliderOpen] = useState<boolean>(false);
+
   return (
-    <header className="z-10 w-full flex items-center justify-center bg-[#121212]">
-      <div className="w-full max-w-screen-2xl flex items-center justify-between px-6 py-3 gap-10">
-        {/* Logo */}
-        <img src="./svg/react.svg" alt="LOGO" className="w-6 md:w-7" />
+    <>
+      <header className="z-10 w-full flex items-center justify-center bg-[#121212]">
+        <div className="w-full max-w-screen-2xl flex items-center justify-between px-6 py-3 gap-10">
+          {/* Logo */}
+          <img src="./svg/react.svg" alt="LOGO" className="w-6 md:w-7" />
 
-        {/* SearchBar */}
-        <div className="relative flex w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl">
-          <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none">
-            <Search className="w-4 h-4 text-muted-foreground md:w-5 md:h-5" />
+          {/* SearchBar */}
+          <div className="relative flex w-full max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl">
+            <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none">
+              <Search className="w-4 h-4 text-muted-foreground md:w-5 md:h-5" />
+            </div>
+
+            <div className="flex flex-1">
+              <Input type="text" placeholder="검색" className="flex-1 pl-10 text-sm md:text-base" />
+              <Button type="submit" variant="outline" className="ml-2 hidden sm:flex">
+                검색
+              </Button>
+            </div>
           </div>
 
-          <div className="flex flex-1">
-            <Input type="text" placeholder="검색" className="flex-1 pl-10 text-sm md:text-base" />
-            <Button type="submit" variant="outline" className="ml-2 hidden sm:flex">
-              검색
-            </Button>
-          </div>
+          {/* MenuButton */}
+          <Button
+            variant="ghost"
+            className="text-white"
+            aria-label="Menu"
+            size="icon-lg"
+            onClick={() => setIsSliderOpen(true)}
+          >
+            <Menu className="size-6 md:size-7" />
+          </Button>
         </div>
+      </header>
 
-        {/* MenuButton */}
-        <Button variant="ghost" className="text-white" aria-label="Menu" size="icon-lg">
-          <Menu className="size-6 md:size-7" />
-        </Button>
-      </div>
-    </header>
+      <AppSlider isSliderOpen={isSliderOpen} onClose={() => setIsSliderOpen(false)} />
+    </>
   );
 }
 
---
+++ src/components/common/AppSlider.tsx
@@ -0,0 +1,60 @@
+import { X } from "lucide-react";
+import { Button } from "../ui";
+import { SLIDER_ITEMS } from "@/constants/AppSliderConstants";
+import { Link, useLocation } from "react-router-dom";
+
+interface AppSliderProps {
+  isSliderOpen: boolean;
+  onClose: () => void;
+}
+
+function AppSlider({ isSliderOpen, onClose }: AppSliderProps) {
+  const location = useLocation();
+
+  return (
+    <>
+      {/* 오버레이 */}
+      <div
+        onClick={onClose}
+        className={`fixed inset-0 bg-black/40 backdrop-blur-sm z-40 transition-opacity duration-300
+          ${isSliderOpen ? "opacity-100 visible" : "opacity-0 invisible"}
+        `}
+      ></div>
+
+      <div
+        className={`bg-[#121212] fixed top-0 left-0 h-full w-64 z-50 transition-transform duration-300
+          ${isSliderOpen ? "translate-x-0" : "-translate-x-full"}
+        `}
+      >
+        <div className="p-4 border-b flex justify-between items-center">
+          <h2 className="text-lg font-bold">카테고리</h2>
+          <Button variant={"ghost"} size={"icon"} onClick={onClose}>
+            <X />
+          </Button>
+        </div>
+
+        <nav className="p-4 flex flex-col gap-3 h-full space-y-3 my-5">
+          {SLIDER_ITEMS.map((item) => {
+            const isActive = location.pathname === item.path;
+
+            return (
+              <Link
+                key={item.id}
+                to={item.path}
+                className={`flex items-center gap-3 rounded-full py-2 hover:bg-white/10 px-3 transition-colors duration-200 
+                  ${isActive ? " text-white" : "text-muted-foreground  hover:text-white"}
+                `}
+                onClick={onClose}
+              >
+                {item.icon}
+                <span>{item.label}</span>
+              </Link>
+            );
+          })}
+        </nav>
+      </div>
+    </>
+  );
+}
+
+export default AppSlider;
