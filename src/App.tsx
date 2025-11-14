54bf0421142d5738bc28fa84d3f7d3d5fedb9e4d
Kimsuro <ss01056489656@gmail.com>
2025-11-14 17:55:24 +0900

    style: App.tsx 공통 페이지 및 컨테이너 스타일 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { AppHeader } from "./components/common";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <div>
-      <AppRoutes />
+    <div className="page">
+      <AppHeader />
+
+      <main className="main_container">
+        <AppRoutes />
+      </main>
+
+      {/* AppFooter 들어갈 예정 */}
     </div>
   );
 }
