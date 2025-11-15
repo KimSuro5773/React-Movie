1c0bf49973a76a7fa6fefa77bb845fe6b29e0818
Kimsuro <ss01056489656@gmail.com>
2025-11-15 17:40:24 +0900

    feat: MovieList 타입 정의 추가

---
+++ src/types/movie.ts
@@ -0,0 +1,27 @@
+export interface MovieListResponse {
+  dates?: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: MovieListItem[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface MovieListItem {
+  adult: boolean;
+  backdrop_path: string | null;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
