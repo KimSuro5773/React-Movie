47e21edb219adb0de92b751c043696be4ecc801b
Kimsuro <ss01056489656@gmail.com>
2025-11-15 18:07:47 +0900

    feat: TMDB API 엔드포인트 정의

---
+++ src/api/endpoints.ts
@@ -0,0 +1,10 @@
+export const END_POINTS = {
+  MOVIES: {
+    NOW_PLAYING: "/movie/now_playing",
+    POPULAR: "/movie/popular",
+    TOP_RATED: "/movie/top_rated",
+    UPCOMING: "/movie/upcoming",
+  },
+
+  MOVIE_DETAIL: (movieId: number) => `/movie/${movieId}`,
+} as const;
