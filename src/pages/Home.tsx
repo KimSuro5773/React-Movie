93d544254841938dd86a7aa6081f455afaf6bd6b
Kimsuro <ss01056489656@gmail.com>
2025-11-16 21:10:56 +0900

    feat: 홈 화면 구현
    - MovieSection에 data를 props로 넘김

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,45 @@
+import { MovieSection } from "@/components/movie";
+import {
+  useNowPlayingMovies,
+  usePopularMovies,
+  useTopRatedMovies,
+  useUpcomingMovies,
+} from "@/hooks/useMovies";
+import { PATHS } from "@/routes/paths";
+
 function Home() {
-  return <div>Home</div>;
+  const { data: nowPlaying } = useNowPlayingMovies(1);
+  const { data: popular } = usePopularMovies(1);
+  const { data: topRated } = useTopRatedMovies(1);
+  const { data: upcoming } = useUpcomingMovies(1);
+
+  return (
+    <div className="container mx-auto space-y-12 py-8">
+      {nowPlaying && (
+        <MovieSection
+          titleHeader="현재 상영중"
+          linkPath={PATHS.NOW_PLAYING}
+          movies={nowPlaying.results}
+        />
+      )}
+
+      {popular && (
+        <MovieSection titleHeader="TMDB 인기" linkPath={PATHS.POPULAR} movies={popular.results} />
+      )}
+
+      {topRated && (
+        <MovieSection
+          titleHeader="최고 평점"
+          linkPath={PATHS.TOP_RATED}
+          movies={topRated.results}
+        />
+      )}
+
+      {upcoming && (
+        <MovieSection titleHeader="개봉 예정" linkPath={PATHS.UPCOMING} movies={upcoming.results} />
+      )}
+    </div>
+  );
 }
 
 export { Home };
