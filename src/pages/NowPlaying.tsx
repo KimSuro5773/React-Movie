49ea02cafb3d715354d44faa3d9367f497489e29
Kimsuro <ss01056489656@gmail.com>
2025-11-17 22:09:14 +0900

    feat: NowPlaying 페이지 구현
    - 로딩 상태일때 GridLayoutSkeleton을 보여줌

--- src/pages/NowPlaying.tsx
+++ src/pages/NowPlaying.tsx
@@ -1,5 +1,18 @@
+import { GridLayout, GridLayoutSkeleton } from "@/components/Layout";
+import { useNowPlayingMovies } from "@/hooks/useMovies";
+import { useState } from "react";
+
 function NowPlaying() {
-  return <div>NowPlaying</div>;
+  const [page, setPage] = useState(1);
+  const { data, isLoading } = useNowPlayingMovies(page);
+
+  if (isLoading) {
+    return <GridLayoutSkeleton />;
+  }
+
+  if (!data) return null;
+
+  return <GridLayout title="현재 상영중" data={data} onPageChange={setPage} />;
 }
 
 export { NowPlaying };
