b45f5f127693b72695b434a7c509c53f9085c934
Kimsuro <ss01056489656@gmail.com>
2025-11-19 07:22:08 +0900

    fix: Layout 폴더를 layout으로 변경하여 대소문자 중복 문제 해결
    - Git 저장소에 Layout과 layout이 중복 등록되어 있던 문제 수정
    
    - Linux 환경에서 대소문자 구분으로 인한 TypeScript 빌드 오류 해결
    
    - TS1149 에러 (파일명이 대소문자만 다름) 해결

--- src/components/Layout/index.ts
+++
@@ -1,6 +0,0 @@
-export { Header } from "./Header/Header";
-
-export { Footer } from "./Footer/Footer";
-
-export { GridLayout } from "./GridLayout/GridLayout";
-export { GridLayoutSkeleton } from "./GridLayout/GridLayoutSkeleton";
