0e567edd20191691fc37df78de52271875ed6ff5
Kimsuro <ss01056489656@gmail.com>
2025-11-19 12:23:26 +0900

    feat: Popular 페이지 구현

--- src/pages/Popular/Popular.tsx
+++ src/pages/Popular/Popular.tsx
@@ -1,5 +1,21 @@
+import { GridLayout, GridLayoutSkeleton } from "@/components/layout";
+import { TMDB_MOVIE_SECTION_TITLE } from "@/features/movies/constants/tmdb";
+import { usePopularMovies } from "@/features/movies/hooks/useMovies";
+import { useState } from "react";
+
 function Popular() {
-  return <div>Popular</div>;
+  const [page, setPage] = useState(1);
+  const { data, isLoading } = usePopularMovies(page);
+
+  console.log(data);
+
+  if (isLoading) {
+    return <GridLayoutSkeleton />;
+  }
+
+  if (!data) return null;
+
+  return <GridLayout title={TMDB_MOVIE_SECTION_TITLE.POPULAR} data={data} onPageChange={setPage} />;
 }
 
 export { Popular };
